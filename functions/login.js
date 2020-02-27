6d02851b0dff1f46eb672bacb316ea8c3eca24bd
Gmatozinho <gustavomatozinho@gmail.com>
2020-02-26 21:22:04 -0300

    create login lambda

--- functions/login.js
+++ functions/login.js
@@ -1,18 +1,35 @@
 'use strict';
 
+const { getUser } = require("./get")
+require('dotenv-safe').config();
+var jwt = require('jsonwebtoken');
+
+/* @params - username,password */
 module.exports.login = async event => {
+  const data = JSON.parse(event.body);
+  const res = await getUser(data.username);
+
+  if (res && res.statusCode === 200 && data.password === res.body.password) {
+    //add id and access group in jwt
+    var token = jwt.sign({ id: res.body.id, access_group: res.body.access_group }, process.env.SECRET, {
+      algorithm: 'HS256',
+      expiresIn: 3000 // expires in 5min
+    });
+
+    return response = {
+      statusCode: 200,
+      body: JSON.stringify({ auth: true, token }),
+    };
+  }
+
   return {
-    statusCode: 200,
+    statusCode: 500,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
+        auth: "false",
+        message: 'Login inválido!',
+      }
     ),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
